c008379c9ffe068710e8b87835b7a4c233e4ef6a
zaid-khan0 <zaid-khan0>
2023-10-31 10:05:39 +0000

    commit

---
+++ Untitled Notebook 2023-10-27 18:16:59 (2).sql
@@ -0,0 +1,18 @@
+-- Databricks notebook source
+create catalog if not exists quickstart;
+
+-- COMMAND ----------
+
+show catalogs;
+
+-- COMMAND ----------
+
+use catalog quickstart;
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+
